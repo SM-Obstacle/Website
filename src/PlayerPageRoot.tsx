01003ca3190d82976c5bc63a81c08b575bac9848
Baptiste Marsigli <baptistemarsigli@gmail.com>
2022-04-12 19:53:07 +0200

    adding some kind of an Easter Egg feature

--- src/PlayerPageRoot.tsx
+++ src/PlayerPageRoot.tsx
@@ -47,7 +47,7 @@ export default function PlayerPageRoot(props: any) {
                     <th>Alt glitch</th>
                     <th>PvP Weapons</th>
                     <th>PvP Collisions</th>
-                    <th>SH</th>
+                    <th class="sh">SH</th>
                 </tr>
             </thead>
             <tbody>
