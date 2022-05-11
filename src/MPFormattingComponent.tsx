0b395389cfeb0ba82963f79d6df52a87874076ff
Baptiste Marsigli <baptistemarsigli@gmail.com>
2022-05-11 23:02:21 +0200

    remove links in mp formatting + placeholder for empty names

--- src/MPFormattingComponent.tsx
+++ src/MPFormattingComponent.tsx
@@ -1,5 +1,15 @@
+function trimMPFormatingLinks(formated) {
+    return formated.replace(/\$i(\[[^\]]*\]?)?/gi, '');
+}
+function trimMPFormating(formated) {
+    return formated.replace(/\$(i(\[[^\]]*\]?)?|[oswnlhpzmg<>]|[0123456789abcdef]{1,3})/gi, '');
+}
 function MPHtml(name) {
-    return { __html:  window.MPStyle.Parser.toHTML(name)};
+    let html = window.MPStyle.Parser.toHTML(trimMPFormatingLinks(name));
+    if (/^\s*$/.test(trimMPFormating(name))) {
+        html = '<del>(unnamed)</del>';
+    }
+    return { __html:  html};
 }
 
 const MPFormattingComponent = ({ name }: { name: string }) => {
