c644e62d6180b637de2b02d28b790467b70efade
Baptiste Marsigli <baptistemarsigli@gmail.com>
2022-11-19 14:59:52 +0100

    add a class to the finish count when it's 100%

--- src/StormRunnerPage.tsx
+++ src/StormRunnerPage.tsx
@@ -122,7 +122,7 @@ export default function StormRunnerPage(props: any) {
 									   }
 								   }}>
 								   <td className="rank">{player.rank}</td>
-								   <td className="finishes">{player.maps_finished}<small>/{map_number}</small></td>
+								   <td className="finishes{player.maps_finished===map_number? " finishes_all" : ""}">{player.maps_finished}<small>/{map_number}</small></td>
 								   <td className="score">{player.score.toFixed(2)}</td>
 								   <td className="name"><a href={`/player/${player.login}`}>{<MPFormattingcomponent name={player.name} placeholder={player.login}/>}</a></td>
 								   <td className="worst">{player.worst.rank}</td>
