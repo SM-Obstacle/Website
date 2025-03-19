a5ff4670aff01639f0be6c77eb0de42cf6f14421
Ahmad Baalbaky <ahmadjfbky@gmail.com>
2025-03-19 19:57:57 +0100

    fix: Some typecheck

--- src/lib/article.ts
+++ src/lib/article.ts
@@ -12,22 +12,22 @@ export type Article = RawArticle & {
   fetchContent: () => Promise<string>,
 };
 
-export type Articles = {
-  [slug: string]: Article;
-};
-
-export async function fetchArticles(): Promise<Articles> {
+export async function fetchArticles(): Promise<{ [slug: string ]: Article }> {
   const file = await fs.readFile(process.cwd() + '/data/articles.json', 'utf-8');
-  const articles: RawArticle = JSON.parse(file);
-  const entries = Object.entries(articles).map(([key, value]) => {
+  const articles: { [slug: string]: RawArticle } = JSON.parse(file);
+
+  return Object.fromEntries(Object.entries(articles).map(([key, raw]) => {
     return [key, {
-      ...value,
-      content: null,
-      fetchContent: async function() {
+      ...raw,
+      // The typechecker seems to struggle to type the `content` field XD
+      content: (<T>(a: T): T => a)<string | undefined>(undefined),
+      fetchContent: async function () {
+        if (this.content) {
+          return this.content;
+        }
         this.content = await fs.readFile(process.cwd() + this.path, 'utf-8');
         return this.content;
       }
     }];
-  });
-  return Object.fromEntries(entries);
+  }));
 }
