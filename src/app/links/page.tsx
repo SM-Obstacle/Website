42114f496a188b9c3f717e4d93053e14d6561c12
Ahmad Baalbaky <ahmadjfbky@gmail.com>
2025-03-19 14:40:22 +0100

    feat: Make website manage its own articles
    Closes #12.

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,36 +1,27 @@
 import { Metadata } from "next";
 import React from "react";
-
-import { fetchGraphql } from "@/lib/utils";
-import { gql } from "../__generated__";
 import Markdown from "react-markdown";
 import { Article, LastUpdate, MdLink } from "@/components/Article";
 import Date from "@/components/Date";
-
-const GET_RESOURCES_CONTENT = gql(/* GraphQL */ `
-  query GetResourcesContent {
-    resourcesContent {
-      content
-      lastModified
-    }
-  }
-`);
+import { fetchArticles } from "@/lib/article";
 
 export const metadata: Metadata = {
   title: "RESOURCES",
 };
 
 export default async function Links() {
-  const content = (await fetchGraphql(GET_RESOURCES_CONTENT)).resourcesContent;
+  const articles = await fetchArticles();
+  const resourcesArticle = articles['__resources__'];
+  const content = await resourcesArticle.fetchContent();
 
   return (
     <Article>
       <div>
         <Markdown components={{
           "a": MdLink
-        }}>{content.content}</Markdown>
+        }}>{content}</Markdown>
       </div>
-      <LastUpdate>Last update: <Date onlyDate>{content.lastModified}</Date></LastUpdate>
+      <LastUpdate>Last update: <Date onlyDate>{resourcesArticle.date}</Date></LastUpdate>
     </Article>
   );
 }
